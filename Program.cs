ceed333f9897181bb453b87c42a726929add1655
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-28 15:16:18 +0530

    Welcome programe

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparison program");
+            Console.WriteLine("Console Option \n 1.Enter the points");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    UC1.GetDistance();
+                    break;
+            }
+
+        }
+    }
+}
