6775f0ca938225e3b6f839cfd110f89cd48d0741
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-28 15:14:39 +0530

    Length of the line

---
+++ UC1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class UC1
+    {
+         public static void GetDistance()
+         {
+           
+            Console.WriteLine("enter the value of x1 ");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of x2 ");
+            double  x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of y1 ");
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of y2 ");
+            double y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Result is " + Result);
+         }
+       
+
+    }
+}
