729e2ecf0effb17068f9c55cdae9390ffc7bf1ac
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-08-29 13:47:42 +0530

    UC1 Updated

--- UC1.cs
+++ UC1.cs
@@ -6,24 +6,31 @@ using System.Threading.Tasks;
 
 namespace LineComparison
 {
-    internal class UC1
+   internal class UC1
     {
-         public static void GetDistance()
+        
+         
+         public static double GetDistance()
          {
-           
+           // Co-ordinates of x
             Console.WriteLine("enter the value of x1 ");
             double x1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter the value of x2 ");
-            double  x2 = Convert.ToInt32(Console.ReadLine());
+            double  x2 = Convert.ToInt32(Console.ReadLine());   
             Console.WriteLine("enter the value of y1 ");
+
+            //Co-ordinates of y
             double y1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter the value of y2 ");
             double y2 = Convert.ToInt32(Console.ReadLine());
 
-            double Result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("Result is " + Result);
+            //Calculating Length between two Co-ordinates
+             Double lengthLine1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of the line 1 is "+lengthLine1);
+             return lengthLine1;    
+            //Console.WriteLine("Length of the line is  " + lengthLine1);
          }
-       
+        
 
     }
 }
